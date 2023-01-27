a25abba609e19107d50b33d6d1e1220dd528f539
Nareen Asad <105769828+NareenAsad@users.noreply.github.com>
2023-01-27 19:02:34 +0500

    Add files via upload

---
+++ 2.29.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int a;
+    a=1;
+    cout<< "Side of square (cm)" << " " << "Perimeter of square (cm)" << " " << "Area of square (cm^2)"<<endl;
+    cout<<a<< "\t" <<4*a<< "\t" <<a*a<<endl;
+    a=a+1;
+    cout<<a<< "\t" <<4*a<< "\t" <<a*a<<endl;
+    a=a+1;
+    cout<<a<< "\t" <<4*a<< "\t" <<a*a<<endl;
+    a=a+1;
+    cout<<a<< "\t" <<4*a<< "\t" <<a*a<<endl;
+    a=a+1;
+    return 0;
+}
+
