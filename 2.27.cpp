d27aa74b851e5b8b1190fc30559ee260a2c3488d
Nareen Asad <105769828+NareenAsad@users.noreply.github.com>
2023-01-28 22:51:03 +0500

    Update 2.27.cpp

--- 2.27.cpp
+++ 2.27.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 using namespace std;
-int main () 
+int main() 
 {
-	cout<<static_cast<int> ('B') <<endl;
-    cout<<static_cast<char> (65) <<endl;
+    cout<<static_cast<int>('B') <<endl;
+    cout<<static_cast<char>(65) <<endl;
     return 0;
 }
-
